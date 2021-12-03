33d658018fdc909644b66c7c51a923c1d650e27a
MazidulIslam <56502659+MazidulIslam@users.noreply.github.com>
2021-12-03 17:07:42 +0600

    create search component

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,16 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 import Layout from "../components/Layout";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head></Head>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
+  );
 }
 
 export default MyApp;
